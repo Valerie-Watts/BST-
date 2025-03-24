91de4f24e6f94d196394589df81e9e082b0957a1
Valerie Watts <valerie.watts.feria@gmail.com>
2025-03-23 20:07:53 -0600

    coms

--- TreeBTS/BTS.cs
+++ TreeBTS/BTS.cs
@@ -9,7 +9,7 @@ namespace TreeBTS
 {
     class BTS
     {
-        public Node root;
+        public Node root; //slay
 
         // Constructor that initializes the BST with a root node containing the given data
         public BTS(int data)
